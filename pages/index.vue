71a22ff3161933442a9252653e7d80a99c0edb9e
Kevin Wagenvoort <kevinwagenvoort@gmail.com>
2022-01-16 13:46:43 +0100

    +1

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,8 @@
     <p>Een poi kun je afvinken dan wordt deze weer dichtgeklapt en wordt de marker groen op de kaart (Dit zou normaal ook naar de api gestuurd worden, zodat je progressie wordt onthouden)</p>
     <p>Route aanmaken bevat een formulier waarbij je alle benodige gegevens kan invoeren (Deze zou dan naar de api worden verstuurd)</p>
     <p>PS: Opmaak ben ik totaal niet goed in, dus ik maak gebruiker voor bootstrap-vue ook heb ik niet een alternatieve opmaak gemaakt voor de telefoon, maar alles werkt nog wel gewoon</p>
+    <h1>Rest opmerkingen</h1>
+    <p>Het was even wennen om vue/nuxt onder de knie te krijgen, maar heb +- 10 uur ingestoken en merk nu al dat ik vloeiender taken uitwerkte</p>
   </div>
 </template>
 
